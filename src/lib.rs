6c34f3f199339050de996e02aaf40ec139e9a4e4
auritron <myself.anath@gmail.com>
2026-06-21 16:24:37 +0530

    made changes to flush and write_char_to_buf, added new functions

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 #![feature(ascii_char)]
 
 mod panichandler;
-mod vga;
+mod vga_buffer;
 
-use vga::*;
-use vga::ForegroundColor::*;
+use vga_buffer::*;
+use vga_buffer::ForegroundColor::*;
 
 const VGA_BUFFER_ADR: *mut u8 = 0xb8000 as *mut u8;
 
@@ -15,21 +15,27 @@ pub extern "C" fn kernel_main() -> ! {
     let message_1 = "Hey, what's up :D!";
     let message_2 = "\nHey, what's up :D! (but it's red)\n";
     let message_3 = "I'm a ";
-    let message_4 = "rust ";
-    let message_5 = "femboy, uwu!";
+    let message_4 = "Rustacean";
+    let message_5 = ", what's up?";
     //let color: u8 = 0x0A;
 
     unsafe {
-        let mut local_buffer = vga::VGABuffer::new();
+        let mut local_buffer = vga_buffer::VGABuffer::new();
+        let vga_ref = 
+            &mut *(VGA_BUFFER_ADR as *mut [[u16; BUFFER_WIDTH]; BUFFER_HEIGHT]);
+
         local_buffer.clear();
         local_buffer.write_plain_text_to_buf(message_1);
         local_buffer.write_fmt_text_to_buf(message_2, Red, None, None);
+
+        local_buffer.flush(vga_ref);
         local_buffer.write_fmt_text_to_buf(message_3, Magenta, None, None);
         local_buffer.write_fmt_text_to_buf(message_4, Yellow, None, None);
+
+        local_buffer.flush(vga_ref);
         local_buffer.write_fmt_text_to_buf(message_5, Magenta, None, None);
 
-        let vga_ref = 
-            &mut *(VGA_BUFFER_ADR as *mut [[u16; BUFFER_WIDTH]; BUFFER_HEIGHT]);
+        
         local_buffer.flush(vga_ref);
     }
 
